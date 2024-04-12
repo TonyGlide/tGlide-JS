b9e0847343a52c3bb4508f028feef2abb645c1b0
TonyGlide <162181740+TonyGlide@users.noreply.github.com>
2024-04-12 23:58:32 +0000

    1

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // let element = document.querySelector(".xyz");
+  // console.log(element);
+  // element.style.color = "red";
+  function updateText(id) {
+    id.innerHTML = "Hi there";
+  }
 };
+
+//   write your code here
+//   let name = "Tony";
+//   let numberArray = [2, 3, 4, 5, 6];
+//   let demoElement = document.querySelector("#demo");
+//   let evenNumbers = numberArray.filter(Element => Element % 2 === 0);
+
+//   for (let Element in evenNumbers) {
+//     let para = document.createElement("p");
+//     para.innerHTML = `Even number is ${evenNumbers[Element]}`;
+//     demoElement.insertAdjacentElement("afterbegin", para);
+//   }
+//   console.log("demoElement");
+// };
