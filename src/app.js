63fe0e96067b1271add14bb387d88e94e0a828d8
TonyGlide <162181740+TonyGlide@users.noreply.github.com>
2024-04-19 23:20:02 +0000

    2

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 window.onload = function() {
-  const sum = require("./app.test");
-
-  test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-  });
+  function sum(a1, b1) {
+    return a1 + b1;
+  }
+  module.exports = sum;
