975cf2596f550130de1c3b3bce7e471c1075f9a8
TonyGlide <162181740+TonyGlide@users.noreply.github.com>
2024-04-19 23:11:48 +0000

    1

--- src/app.js
+++ src/app.js
@@ -1,29 +1,6 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  // let element = document.querySelector(".xyz");
-  // console.log(element);
-  // element.style.color = "red";
-  function updateText(id) {
-    id.innerHTML = "Hi there";
-  }
-};
+  const sum = require('../src/app.test');
 
-//   write your code here
-//   let name = "Tony";
-//   let numberArray = [2, 3, 4, 5, 6];
-//   let demoElement = document.querySelector("#demo");
-//   let evenNumbers = numberArray.filter(Element => Element % 2 === 0);
-
-//   for (let Element in evenNumbers) {
-//     let para = document.createElement("p");
-//     para.innerHTML = `Even number is ${evenNumbers[Element]}`;
-//     demoElement.insertAdjacentElement("afterbegin", para);
-//   }
-//   console.log("demoElement");
-// };
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
